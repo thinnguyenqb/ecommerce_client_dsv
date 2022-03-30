625f8c5db273dc0765e0af29613e5b540be97d65
EricNguyen99 <vanthin7111999@gmail.com>
2022-03-30 17:07:56 +0700

    add: change password

--- src/components/customer/FormChangePassword/FormChangePassword.jsx
+++ src/components/customer/FormChangePassword/FormChangePassword.jsx
@@ -1,42 +1,102 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Input, Button } from "antd";
 import "./FormChangePassword.scss";
+import { useDispatch } from 'react-redux';
+import { changePassword } from './../../../redux/actions/profileAction';
 
 export function FormChangePassword() {
+  const [form] = Form.useForm();
+  const [, forceUpdate] = useState({}); // To disable submit button at the beginning.
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    forceUpdate({});
+  }, []);
+
+  const onFinish = async (values) => {
+    const data = {
+      cur_password: values.cur_password,
+      password: values.password
+    }
+
+    await dispatch(changePassword(data))
+
+  };
   return (
     <div className="form-change-password">
-      <Form>
-        <Form.Item className="input-content" label="CURRENT PASSWORD">
-          <Input
-            className="input-login"
-            placeholder="Enter your password..."
-          />
-        </Form.Item>
-
-        <Form.Item className="input-content" label="NEW PASSWORD">
-          <Input
-            className="input-login"
-            type="password"
-            placeholder="Enter your password..."
-          />
-        </Form.Item>
-
-        <Form.Item className="input-content" label="RE-ENTER PASSWORD">
-          <Input
+      <Form form={form} name="horizontal_login" onFinish={onFinish}>
+        <Form.Item
+          className="input-content"
+          label="CURRENT PASSWORD"
+          name="cur_password"
+          rules={[
+            { required: true, message: "Please input your password!" },
+            { min: 6, message: "Password must be minimum 6 characters." },
+          ]}
+        >
+          <Input.Password
             className="input-login"
             type="password"
             placeholder="Enter your password..."
           />
         </Form.Item>
 
-        <Form.Item className="footer">
+        <Form.Item
+          className="input-content"
+          label="PASSWORD"
+          name="password"
+          rules={[
+            { required: true, message: "Please input your password!" },
+            { min: 6, message: "Password must be minimum 6 characters." },
+          ]}
+        >
+          <Input.Password
+            className="input-login"
+            type="password"
+            placeholder="Enter your password..."
+          />
+        </Form.Item>
+
+        <Form.Item
+          className="input-content"
+          label="RE-ENTER PASSWORD"
+          name="cf_password"
+          dependencies={["password"]}
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("The two passwords that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password placeholder="Confirm password" />
+        </Form.Item>
+
+        <Form.Item shouldUpdate className="footer">
+        {() => (
           <Button
             type="primary"
-            htmlType="submit"
-            className="primary-form-button"
+              htmlType="submit"
+              className="primary-form-button"
+            disabled={
+              !form.isFieldsTouched(true) ||
+              !!form.getFieldsError().filter(({ errors }) => errors.length).length
+            }
           >
             Save
           </Button>
+        )}
         </Form.Item>
       </Form>
     </div>
--- src/redux/actions/profileAction.js
+++ src/redux/actions/profileAction.js
@@ -1,7 +1,35 @@
+import { GLOBALTYPES } from "./globalTypes";
+import axios from "axios";
 
 export const PROFILE_TYPES = {
   UPDATE_PROFILE: "UPDATE_PROFILE",
+  CHANGE_PASSWORD: "CHANGE_PASSWORD",
 };
 
 export const updateProfileUser = ({userData, auth}) => async (dispatch) => {
 }
+
+export const changePassword = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } })
+    const token = localStorage.getItem("access_token")
+    const res = await axios.post(process.env.REACT_APP_API_URL + '/user/change', data, {
+      headers: {Authorization: token} 
+  })
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        success: res.data.msg
+      }
+    })
+  
+  } catch (err) {
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        error: err.response.data.msg
+      }
+    })
+  }
+}
+
